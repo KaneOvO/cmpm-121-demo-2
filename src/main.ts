197ad377b91e850bf9b60256163aba3526aa3922
Zexuan <lzx213@sina.com>
2023-10-17 11:32:23 -0700

    Finished Step 1

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,18 @@ import "./style.css";
 
 const app: HTMLDivElement = document.querySelector("#app")!;
 
-const gameName = "My game";
+const gameName = "Zexuan's Sticker Sketchpad";
 
 document.title = gameName;
 
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.style.border = "thin solid black";
+canvas.style.borderRadius = "10px";
+canvas.style.boxShadow = "0px 0px 10px rgba(0, 0, 0, 0.5)";
+app.append(canvas);
